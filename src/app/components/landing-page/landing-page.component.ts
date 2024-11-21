6b27dceecc689fb64646471bbf2ca4d8787733e9
Darian <oriaperezdarian@gmail.com>
2024-11-20 22:59:24 -0500

    diseno 1.0.0

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
@@ -8,5 +8,41 @@ import { Component } from '@angular/core';
   styleUrl: './landing-page.component.css'
 })
 export class LandingPageComponent {
+  images = [
+    {id:1, url: 'assets/img/image00001.jpeg', alt: 'Image 1'},
+    {id:2, url: 'assets/img/image00002.jpeg', alt: 'Image 2'},
+    {id:3, url: 'assets/img/image00003.jpeg', alt: 'Image 3'},
+    {id:4, url: 'assets/img/image00004.jpeg', alt: 'Image 4'},
+    {id:5, url: 'assets/img/image00005.jpeg', alt: 'Image 5'},
+    {id:6, url: 'assets/img/image00006.jpeg', alt: 'Image 6'},
+    {id:7, url: 'assets/img/image00007.jpeg', alt: 'Image 7'},
+    {id:8, url: 'assets/img/image00008.jpeg', alt: 'Image 8'},
+    {id:9, url: 'assets/img/image00009.jpeg', alt: 'Image 9'},
+    {id:10, url: 'assets/img/image00010.jpeg', alt: 'Image 10'},
+    {id:11, url: 'assets/img/image00011.jpeg', alt: 'Image 11'},
+    {id:12, url: 'assets/img/image00012.jpeg', alt: 'Image 12'},
+    {id:13, url: 'assets/img/image00013.jpeg', alt: 'Image 13'},
+    {id:14, url: 'assets/img/image00014.jpeg', alt: 'Image 14'},
+    {id:15, url: 'assets/img/image00015.jpeg', alt: 'Image 15'},
+    {id:16, url: 'assets/img/image00016.jpeg', alt: 'Image 16'},
+    {id:17, url: 'assets/img/image00017.jpeg', alt: 'Image 17'},
+    {id:18, url: 'assets/img/image00018.jpeg', alt: 'Image 18'},
+    {id:19, url: 'assets/img/image00019.jpeg', alt: 'Image 19'},
+    {id:20, url: 'assets/img/image00020.jpeg', alt: 'Image 20'},
+    {id:21, url: 'assets/img/image00021.jpeg', alt: 'Image 21'},
+    {id:22, url: 'assets/img/image00022.jpeg', alt: 'Image 22'},
+  ];
+  currentIndex: number = 0;
+
+  ngOnInit(): void {}
+
+  next() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  prev() {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
+
 
 }
